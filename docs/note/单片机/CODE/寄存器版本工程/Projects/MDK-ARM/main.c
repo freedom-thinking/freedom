8d59fe4454c4a008ded0f6eda1603fa323013003
flower <12756957+flower-blooming@user.noreply.gitee.com>
2026-03-29 16:23:28 +0800

    更新笔记CAD和单片机

---
+++ docs/note/单片机/CODE/寄存器版本工程/Projects/MDK-ARM/main.c
@@ -0,0 +1,18 @@
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./SYSTEM/delay/delay.h"
+
+int main(void){
+    uint8_t t = 0;
+    sys_stm32_clock_init(9);/*设置时钟，72Mhz*/
+    delay_init(72);/*延时初始化*/
+    usart_init(72,115200);/**/
+    while (1)
+{
+    printf("t:%d\r\n",t);
+    delay_ms(500);
+    t++;
+}
+
+}
+
